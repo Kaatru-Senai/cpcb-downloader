7b833ff99c2900d4cfb49b4a41c8a9aa5f758b05
suvindran3 <suvindran2001@gmail.com>
2022-09-14 08:16:12 +0530

    implement retry, validate response data

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import datetime
+import os.path
 import time
 import argparse
 import pandas
 from cpcb_station_raw_data_parser import get_site_list, CpcbParam
 from payload import Payload
 import requests
+from requests.exceptions import Timeout
 from model.response_data import ParseData
 
 
-def get_data():
-    pd: pandas.DataFrame = pandas.DataFrame()
+def start_process():
+    if os.path.exists('cpcb-data.csv'):
+        pd: pandas.DataFrame = pandas.read_csv('cpcb-data.csv')
+    else:
+        pd: pandas.DataFrame = pandas.DataFrame()
     stations_list = get_site_list()
     for station in stations_list:
         for k, v in station.items():
-            payload = Payload(state=v[CpcbParam.STATE_NAME], city=v[CpcbParam.CITY_NAME], site_id=k,
-                              start_date=from_date, end_date=to_date).generate()
-            response = requests.post('https://app.cpcbccr.com/caaqms/fetch_table_data',
-                                     data=payload, headers=headers)
-            pd = pandas.concat([pd, pandas.DataFrame.from_dict(ParseData(response.json(), v).get())], axis=0,
+            pd = pandas.concat([pd, pandas.DataFrame.from_dict(get_cpcb_data(k, v))], axis=0,
                                ignore_index=True)
         pd.to_csv('cpcb-data.csv', index=False)
         print(len(pd))
         time.sleep(1)
+
+
+def get_cpcb_data(site_id: str, site_meta_data: dict):
+    global retry_sleep_time
+    payload = Payload(state=site_meta_data[CpcbParam.STATE_NAME], city=site_meta_data[CpcbParam.CITY_NAME],
+                      site_id=site_id, start_date=from_date, end_date=to_date).generate()
+    try:
+        response = requests.post('https://app.cpcbccr.com/caaqms/fetch_table_data',
+                                 data=payload, headers=headers, timeout=10)
+        retry_sleep_time = 0
+        try:
+            return ParseData(response.json(), site_meta_data).get()
+        except ValueError:
+            print(site_id)
+            print(response.text)
+    except Timeout:
+        retry_sleep_time += 3
+        time.sleep(retry_sleep_time)
 
 
 headers = {
@@ -46,6 +65,7 @@ headers = {
                     '104.0.0.0Safari/537.36 '
 
 }
+retry_sleep_time = 0
 parser = argparse.ArgumentParser()
 parser.add_argument(
     '-fd', help='enter the from datetime in following format dd-mm-yyyy hh:mm (use 24H)')
@@ -61,10 +81,10 @@ if args.fd or args.td:
                 print('ERROR: to datetime should not exceed present datetime')
             else:
                 print('Fetching data')
-                get_data()
+                start_process()
         else:
             print('ERROR: from date should come before to date')
-    except ValueError:
+    except ValueError as err:
         print('ERROR: entered date format is not right')
 else:
     print('ERROR: required arguments missing')
